4e20aad654693f907cda27f03c4f6be1e1793f5d
NadaBenAissa21 <nada@semwee.com>
2021-11-04 00:06:55 +0100

    add log4j for Entreprise entity and deploy nexus

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -3,6 +3,7 @@ package tn.esprit.spring.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,35 +21,83 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 	
+	private static final Logger l = Logger.getLogger(EntrepriseServiceImpl.class);
+
+	
 	public int ajouterEntreprise(Entreprise entreprise) {
-		entrepriseRepoistory.save(entreprise);
+		try{
+			l.debug("Je vais  lancer lancer l'ajout de l'entreprise");
+			entrepriseRepoistory.save(entreprise);
+			l.info("Entreprise est ajouté avec succès");
+		}
+		catch(Exception e){
+			l.error("Erreur dans l'ajout de l'entreprise ajouterEntreprise()!!"+ e);
+		}
+		finally{
+			l.info("la méthode ajouterEntreprise() est finie");
+		}
 		return entreprise.getId();
 	}
 
 	public int ajouterDepartement(Departement dep) {
-		deptRepoistory.save(dep);
+		try{
+			l.debug("Je vais  lancer lancer l'ajout d'une département");
+			deptRepoistory.save(dep);
+			l.info("Département est ajoutée avec succès");
+		}
+		catch(Exception e){
+			l.error("Erreur dans la méthode ajouterDepartement()!!"+ e);
+		}
+		finally{
+			l.info("la méthode ajouterDepartement() est finie");
+		}
 		return dep.getId();
 	}
 	
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
 		//Le bout Master de cette relation N:1 est departement  
-				//donc il faut rajouter l'entreprise a departement 
-				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
-				//Rappel : la classe qui contient mappedBy represente le bout Slave
-				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
-				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-				Departement depManagedEntity = deptRepoistory.findById(depId).get();
+		//donc il faut rajouter l'entreprise a departement 
+		// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
+		//Rappel : la classe qui contient mappedBy represente le bout Slave
+		//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
+
+		try{
+			l.debug("Je vais  lancer affecterDepartementAEntreprise");
+			Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+			Departement depManagedEntity = deptRepoistory.findById(depId).get();
+			depManagedEntity.setEntreprise(entrepriseManagedEntity);
+			deptRepoistory.save(depManagedEntity);
+			
+			l.info("Département est affectée à une entreprise avec succès");
+		}
+		catch(Exception e){
+			l.error("Erreur dans la méthode affecterDepartementAEntreprise()!!"+ e);
+		}
+		finally{
+			l.info("la méthode affecterDepartementAEntreprise() est finie");
+		}
+
 				
-				depManagedEntity.setEntreprise(entrepriseManagedEntity);
-				deptRepoistory.save(depManagedEntity);
+
 		
 	}
 	
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
-		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 		List<String> depNames = new ArrayList<>();
-		for(Departement dep : entrepriseManagedEntity.getDepartements()){
-			depNames.add(dep.getName());
+		try{
+			l.debug("Je vais lancer getAllDepartementsNamesByEntreprise()");
+			Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+			for(Departement dep : entrepriseManagedEntity.getDepartements()){
+				depNames.add(dep.getName());
+			}
+			
+			l.info("la méthode getAllDepartementsNamesByEntreprise est réalisé avec succès");
+		}
+		catch(Exception e){
+			l.error("Erreur dans la méthode getAllDepartementsNamesByEntreprise()!!"+ e);
+		}
+		finally{
+			l.info("la méthode getAllDepartementsNamesByEntreprise() est finie");
 		}
 		
 		return depNames;
@@ -56,17 +105,55 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 
 	@Transactional
 	public void deleteEntrepriseById(int entrepriseId) {
-		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+		try{
+			l.debug("lancement deleteEntrepriseById()");			
+			
+			entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+			
+			l.info("la méthode deleteEntrepriseById() est réalisée avec succès");
+		}
+		catch(Exception e){
+			l.error("Erreur dans la méthode deleteEntrepriseById()!!"+ e);
+		}
+		finally{
+			l.info("la méthode deleteEntrepriseById() est finie");
+		}
+		
 	}
 
 	@Transactional
 	public void deleteDepartementById(int depId) {
-		deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+		try{
+			l.debug("lancement deleteDepartementById()");			
+			
+			deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+
+			l.info("la méthode deleteDepartementById() est réalisée avec succès");
+		}
+		catch(Exception e){
+			l.error("Erreur dans la méthode deleteDepartementById()!!"+ e);
+		}
+		finally{
+			l.info("la méthode deleteDepartementById() est finie");
+		}
 	}
 
 
 	public Entreprise getEntrepriseById(int entrepriseId) {
-		return entrepriseRepoistory.findById(entrepriseId).get();	
+		Entreprise entreprise = null ;
+		try{
+			l.debug("lancement getEntrepriseById()");			
+			entreprise = entrepriseRepoistory.findById(entrepriseId).get();
+			l.info("la méthode getEntrepriseById() est réalisée avec succès");
+
+		}
+		catch(Exception e){
+			l.error("Erreur dans la méthode getEntrepriseById()!!"+ e);
+		}
+		finally{
+			l.info("la méthode getEntrepriseById() est finie");
+		}
+		return entreprise;
 	}
 
 }
--- src/test/java/tn/esprit/spring/EntrepriseTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.spring;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,51 +10,131 @@ import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.controller.RestControlEntreprise;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.services.IEntrepriseService;
 @SpringBootTest()
 @RunWith(SpringRunner.class)
 public class EntrepriseTest {
 
 	@Autowired
 	RestControlEntreprise controlEntreprise;
-	@Autowired
-	IEntrepriseService ientrepriseservice;
+	
+	private static final Logger l = Logger.getLogger(EntrepriseTest.class);
 	
 	@Test
 	 public void testajouterEntreprise(){
 		 Entreprise entreprise= new Entreprise (10,"Sagem", "Sagemcom");
-		 controlEntreprise.ajouterEntreprise(entreprise); 
+			try{
+				l.debug("Je vais  lancer lancer test ajout de l'entreprise");
+				 controlEntreprise.ajouterEntreprise(entreprise); 
+				l.info("Entreprise est ajouté avec succès");
+			}
+			catch(Exception e){
+				l.error("Erreur dans testajouterEntreprise()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testajouterEntreprise() est finie");
+			}		 
 	 }
 	 
 	 @Test
 	 public void testGetEntrepriseById(){
-		 controlEntreprise.getEntrepriseById(10);
 		 
+		 try{
+				l.debug("lancement testgetEntrepriseById()");			
+				 controlEntreprise.getEntrepriseById(10);
+				l.info("la méthode testgetEntrepriseById() est réalisée avec succès");
+
+			}
+			catch(Exception e){
+				l.error("Erreur dans la méthode getEntrepriseById()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testgetEntrepriseById() est finie");
+			}
+		 		 
 	 }
 	 
 	 @Test
 	 public void testAjouterDepartement(){
-		 Departement departement = new Departement (10,"Informatique");
-		 controlEntreprise.ajouterDepartement(departement);
+		 Departement departement = new Departement (10,"Informatique");	 
+			try{
+				l.debug("Je vais  lancer lancer test ajout d'une département");
+				 controlEntreprise.ajouterDepartement(departement);
+				l.info("Département est ajoutée avec succès");
+			}
+			catch(Exception e){
+				l.error("Erreur dans la méthode testAjouterDepartement()()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testAjouterDepartement() est finie");
+			}
 	 }
 	 
 	 @Test
 	 public void testAffecterDepartementAEntreprise(){
-		 controlEntreprise.affecterDepartementAEntreprise(5, 10); //idDep,idEnt
+			try{
+				l.debug("Je vais  lancer testAffecterDepartementAEntreprise");
+				
+				 controlEntreprise.affecterDepartementAEntreprise(5, 10); //idDep,idEnt
+
+				l.info("Département est affectée à une entreprise avec succès");
+			}
+			catch(Exception e){
+				l.error("Erreur dans la méthode testAffecterDepartementAEntreprise()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testAffecterDepartementAEntreprise() est finie");
+			}		 
+		 
+		 
+		 
 	 }
 	
 	 @Test
-	 public void testGetAllDepartementsNamesByEntreprise(){
-		 controlEntreprise.getAllDepartementsNamesByEntreprise(10);
+	 public void testGetAllDepartementsNamesByEntreprise(){		 
+			try{
+				l.debug("Je vais lancer testGetAllDepartementsNamesByEntreprise()");
+				 controlEntreprise.getAllDepartementsNamesByEntreprise(10);
+				l.info("la méthode testGetAllDepartementsNamesByEntreprise() est réalisé avec succès");
+			}
+			catch(Exception e){
+				l.error("Erreur dans la méthode testGetAllDepartementsNamesByEntreprise()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testGetAllDepartementsNamesByEntreprise() est finie");
+			}
 	 }
 	 
 	 @Test
 	 public void testDeleteDepartementById(){
-		 controlEntreprise.deleteDepartementById(6);
+			try{
+				l.debug("lancement testDeleteDepartementById()");			
+				
+				 controlEntreprise.deleteDepartementById(7);
+				
+				l.info("la méthode testDeleteDepartementById() est réalisée avec succès");
+			}
+			catch(Exception e){
+				l.error("Erreur dans la méthode testDeleteDepartementById()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testDeleteDepartementById() est finie");
+			}
 	 }
 	 
 	 @Test
-	 public void testDeleteEntrepriseById(){
-		 controlEntreprise.deleteEntrepriseById(11);
+	 public void testDeleteEntrepriseById(){		 
+			try{
+				l.debug("lancement testDeleteEntrepriseById()");			
+				
+				 controlEntreprise.deleteEntrepriseById(12);
+				
+				l.info("la méthode testDeleteEntrepriseById() est réalisée avec succès");
+			}
+			catch(Exception e){
+				l.error("Erreur dans la méthode testDeleteEntrepriseById()!!"+ e);
+			}
+			finally{
+				l.info("la méthode testDeleteEntrepriseById() est finie");
+			}
 	 }
 }
