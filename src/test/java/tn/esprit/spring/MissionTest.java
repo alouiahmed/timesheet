eec8218d3f6fd4f3fef5d7d2b2dfa5761586ae36
alouiahmed <ahmed.aloui@esprit.tn>
2021-11-11 11:36:07 +0100

    correction de classe de test de l'entité Mission

--- src/test/java/tn/esprit/spring/MissionTest.java
+++ src/test/java/tn/esprit/spring/MissionTest.java
@@ -1,109 +1,97 @@
 package tn.esprit.spring;
 
-import java.util.ArrayList;
 import static org.junit.Assert.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.controller.RestControlTimesheet;
+import tn.esprit.spring.controller.RestControlEntreprise;
 import tn.esprit.spring.entities.Mission;
-import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.repository.MissionRepository;
-import tn.esprit.spring.services.ITimesheetService;
-import tn.esprit.spring.services.IEntrepriseService;
+
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest()
 public class MissionTest {
+
+
+	private static final Logger l = LogManager.getLogger(MissionTest.class);	
+
+	@Autowired
+	RestControlTimesheet restControlTimesheet;
 	
 	@Autowired
-	ITimesheetService service1;
-	
-	@Autowired
-	IEntrepriseService service2;
-	
-	@Autowired
-	DepartementRepository drep;
-	
+	RestControlEntreprise restControlEntreprise;
+	 
 	@Autowired
 	MissionRepository rep;
-	
-	private static final Logger l = LogManager.getLogger(MissionTest.class);	
-	
-	
+
+
 	@Test
 	public void testAjouterMission()
 	{
+		Mission mission=new Mission("testAjout", "testAjout");
 		try {
 			l.info("In testAjouterMission():");
-			Mission mission=new Mission("testAjout", "testAjout");
-			int id=service1.ajouterMission(mission);
-			assertNotNull(id);
+			int idmission=restControlTimesheet.ajouterMission(mission);
+			
+			l.info("Tester que l'id de mission est not null");
+			assertNotNull(idmission);
+			
+			l.info("Tester que la longueur de Nom est supérieur à 0");
 			assertTrue(mission.getName().length() >0);
+
+			l.info("Tester que la longueur de Description est supérieur à 0");
 			assertTrue(mission.getDescription().length() >0);
-			ArrayList <Mission> liste1 =(ArrayList<Mission>) rep.findAll();
-			int size1=liste1.size();
-			l.info(()->"nombre de missions avant l'ajout: " +size1);
-			l.info("Je vais ajouter une mission.");
-			ArrayList <Mission> liste2 =(ArrayList<Mission>) rep.findAll();
-			int size2=liste2.size();
-			l.info(()->"nombre de missions apres l'ajout: " +size2);
-			l.info("comparaison size avant et apres.");
-			assertTrue(size2==size1+1);
-			l.info("Out testAjouterEntreprise() sans erreurs.");
+			
+			l.info("Out testAjouterMission() sans erreurs.");
+			
+			
+
 		}
 		catch (Exception e)
-		{ l.error(()->"Erreur dans testAjouterEntreprise() : " + e); }
+		{ l.error(()->"Erreur dans testAjouterMission() : " + e); }
 	}
 	
-	
 	@Test
-	public void testAffecterMission()
+	public void testaffecterMissionADepartement()
 	{
 		try {
-			l.info("In testAffecterMission():");
+
+			l.info("In testAffecterMissionADepartement():");
 			l.info("Je vais creer un Departement.");
 			Departement departement=new Departement("Ressources Humaines.");
 			l.info("Je vais creer une mission.");
 			Mission mission=new Mission("testAjout", "testAjout");
 			l.info("Je vais ajouter le departement.");
-			int ajouterdepartement=service2.ajouterDepartement(departement);
+			int ajouterdepartement=restControlEntreprise.ajouterDepartement(departement);
 			l.info("Je vais ajouter la mission.");
-			int d=service1.ajouterMission(mission);
+			int d=restControlTimesheet.ajouterMission(mission);
 			l.info("Je vais affecter la mission au departement.");
-			service1.affecterMissionADepartement(d, ajouterdepartement);
+			restControlTimesheet.affecterMissionADepartement(d, ajouterdepartement);
 			l.info("Je vais reprendre la mission depuis la base de donnée.");
 			Mission findbyid=rep.findById(d).orElseThrow(RuntimeException::new);
 			l.info("Je vais tester si le departement_id du mission est égale a l'id de departement auquel je l'ai affecté.");
 			assertTrue(findbyid.getDepartement().getId()==ajouterdepartement);
 			l.info("Out testAffecterDepartement() sans erreurs.");
-		}catch (Exception e) { l.error(()-> "Erreur dans testAffecterDepartement() : " + e); }
-	}}
+			
+			
+			
+		}
+		catch (Exception e)
+		{ l.error(()->"testaffecterMissionADepartement() : " + e); }
+	}
+
+
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
