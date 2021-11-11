00c743fad95ab98c86dfc3d46ffe19e886e766b2
Aymen <aymen.ouerghui@esprit.tn>
2021-11-11 18:33:11 +0100

    Docker

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -38,72 +38,8 @@ class TimesheetApplicationTests {
 	ITimesheetService itimesheetService;
 	@Test
 	void contextLoads() {
-		//employee test
-		Employe emp = new Employe("Ouerghui","Aymen","aymen.ouerghui@esprit.tn","123456",true,Role.INGENIEUR);
-		iEmployeService.addOrUpdateEmploye(emp);
 		
-		//iEmployeService.authenticate("aymen.ouerghui@esprit.tn", "123");
-		Employe loggedIn= iEmployeService.authenticate("aymen.ouerghui@esprit.tn", "123456");
-		
-		//iEmployeService.deleteEmployeById(6);
-		//iEmployeService.mettreAjourEmailByEmployeId("testemail@email.com", 7);
-		
-		
-		iEmployeService.getEmployePrenomById(loggedIn.getId());
-		iEmployeService.getNombreEmployeJPQL();
-		iEmployeService.getAllEmployeNamesJPQL();
-		
-		
-		//departement test
-        Departement department = new Departement();
-        depRep.save(department);
-        iEmployeService.affecterEmployeADepartement(loggedIn.getId(),department.getId());
-		//iEmployeService.desaffecterEmployeDuDepartement(11,3);
-		
-		
-		//contrat test
-		Contrat contrat = new Contrat();
-		iEmployeService.ajouterContrat(contrat);
-		iEmployeService.affecterContratAEmploye(contrat.getReference(),loggedIn.getId());
-		//iEmployeService.deleteContratById(2);
-		
-		
-
-
-
-
-
-
-		Entreprise e = new Entreprise("ENTREPRISENAME", "MITHEL");
-		ientrepriseservice.ajouterEntreprise(e);
-        Departement department1 = new Departement();
-		
-		department.setName("IT");
-		ientrepriseservice.ajouterDepartement(department1);
-		
-		Departement department2 = new Departement();
-		//d1.setEntreprise(e);
-		department1.setName("TELECOM");
-		ientrepriseservice.ajouterDepartement(department2);
-		
-		ientrepriseservice.affecterDepartementAEntreprise(1, 1);
-		ientrepriseservice.affecterDepartementAEntreprise(2, 1);
-	//-------------------------------TIMESHEET-------------------------------------------
-		
-		
-		//timesheet test
-		
-		Date firstDate = new Date();
 	
-		Mission mission = new Mission("mission impossible", "c est une mission tres difficle");
-		itimesheetService.ajouterMission(mission);
-
-				itimesheetService.ajouterTimesheet(1, 1, firstDate,firstDate);
-				
-				itimesheetService.getAllEmployeByMission(1);
-				itimesheetService.affecterMissionADepartement(1, 1);
-			
-				itimesheetService.findAllMissionByEmployeJPQL(1);
 				
 	}
 
